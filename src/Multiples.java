3123736642ce943defa8869d834abd6b178d0571
Akunna <akunna.megwa@gmail.com>
2023-09-11 18:19:52 -0400

    counts multiples of 3 and 5 in a given n

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 1000; i++){
+            boolean multipleof3 = i % 3 == 0;
+            boolean multipleof5 = i % 5 == 0;
+            if (multipleof3 || multipleof5) {
+                count += 1;
+            }
+        }
+        System.out.println(count);
+    }
 }
