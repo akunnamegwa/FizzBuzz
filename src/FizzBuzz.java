21d7c0afa101b52e8c45b22030d03cf419553b16
Akunna <akunna.megwa@gmail.com>
2023-09-11 14:28:51 -0400

    worked on calling private methods from FizzBuzz here

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,34 +1,42 @@
 /**
  * Solve the FizzBuzz challenge.
  */
+
 class FizzBuzz {
-
     public static void main(String[] args) {
-
-        for (int i = 1; i < 100; i++) {
-
+        int j = 1;
+        while (j < 100) {
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+            j = doFizzByzz(j);
         }
     }
+
+    private static int doFizzByzz(int j) {
+        boolean divisibleBy3 = j % 3 == 0;
+        boolean divisibleBy5 = j % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+            j++;
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+            j++;
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+            j++;
+
+        } else {
+
+            System.out.println(j);
+            j++;
+        }
+        return j;
+    }
 }
+
